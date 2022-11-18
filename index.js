4bea58b00f377aa1f85b5c21efad5e7d5a50399b
drib <art.drib@gmail.com>
2022-11-18 13:29:33 +0545

    added robots.txt to discourage site crawlers

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
 
+app.use(express.static("public"));
+
 
 // All the API routes
 app.get("/", (req, res)=>{
